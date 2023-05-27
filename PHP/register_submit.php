66d20da5fd4d18cdf559d3c96bb2f538ee434f69
UTKARSH-SAH <utkarshsah2001@gmail.com>
2023-05-27 15:45:24 +0530

    new

---
+++ PHP/register_submit.php
@@ -0,0 +1,10 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    echo $name . "<br/>"; 
+    echo $email . "<br />";
+    echo $password . "<br />"; 
+    ?>
+  
